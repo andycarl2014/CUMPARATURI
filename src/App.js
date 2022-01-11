374edecc6ce7b7a8ce1d0bb1ef54794ea4a61440
andycarl2014 <innocentskz42@gmail.com>
2022-01-11 11:07:22 +0200

    dupa comentarii

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ export default class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			obiecte: [],
+			obiecte: [], // Array with all the objects , bought and not bought
 			currentItem: {
+				// Current item to be added in the array
 				name: '',
 				quantity: null,
 			},
@@ -18,44 +19,52 @@ export default class App extends Component {
 	}
 
 	handleChange(key) {
+		// Sets the state to completed or not completed based on the last state
 		this.setState((prevState) => {
 			const updatedObjects = prevState.obiecte.map((item) => {
 				if (item.key === key) {
+					// If key of the item in the array is === to the key of the Item to be modified, the completed property is changed
 					item.completed = !item.completed;
 				}
-				return item;
+				return item; // Returns the modified item
 			});
-			return { obiecte: updatedObjects };
+			return { obiecte: updatedObjects }; // Returns the modified state with the modified item
 		});
 	}
 	handleClickPlus(key) {
+		// Modifies the quantity of the item
 		this.setState((prevState) => {
 			const updatedObjects = prevState.obiecte.map((item) => {
 				if (item.key === key) {
-					item.quantity = item.quantity + 1;
+					// If key of the item in the array is === to the key of the Item to be modified
+					item.quantity = item.quantity + 1; // Increases quantity by 1
 				}
-				return item;
+				return item; // Returns the modified item
 			});
-			return { obiecte: updatedObjects };
+			return { obiecte: updatedObjects }; // Returns the modified state with the modified item
 		});
 	}
 	handleClickMinus(key) {
+		// Modifies the quantity of the item
 		this.setState((prevState) => {
 			const updatedObjects = prevState.obiecte.map((item) => {
 				if (item.key === key) {
-					item.quantity = item.quantity - 1;
+					// If key of the item in the array is === to the key of the Item to be modified
+					item.quantity = item.quantity - 1; // Decreases quantity by 1
 				}
 
-				return item;
+				return item; // Returns the modified item
 			});
 			const updatedObjects2 = updatedObjects.filter(
+				// If the quantity of the item is less than 0 it is deleted from the array
 				(item) => item.quantity > 0
 			);
-			return { obiecte: updatedObjects2 };
+			return { obiecte: updatedObjects2 }; // Returns the modified state with the modified item
 		});
 	}
 	handleClick2() {
 		const itemToAdd = {
+			// Current item from the input fields
 			key: this.state.obiecte.length,
 			name: this.state.currentItem.name,
 			quantity: parseInt(this.state.currentItem.quantity) || 0,
@@ -63,9 +72,10 @@ export default class App extends Component {
 		};
 		if (
 			this.state.obiecte.filter((item) => item.name === itemToAdd.name)
-				.length === 0
+				.length === 0 // If the item has not been already added to the array checks the inputs
 		) {
 			if (itemToAdd.name !== '' && itemToAdd.quantity > 0)
+				// If the inputs are completed it adds the item using setState
 				this.setState((prevState) => ({
 					obiecte: [...prevState.obiecte, itemToAdd],
 				}));
@@ -78,15 +88,15 @@ export default class App extends Component {
 				<div className='deCumparat'>
 					<h1>
 						{
-							this.state.obiecte
-								.filter((item) => item.completed === false)
-								.filter((item) => item.quantity > 0).length
+							this.state.obiecte.filter((item) => item.completed === false)
+								.length // Number of the items not bought
 						}{' '}
 						de cumparat
 					</h1>
 					{this.state.obiecte
 						.filter((item) => item.completed === false)
 						.map((item, index) => {
+							// Returns an Item for each uncompleted item in the array
 							return (
 								<Item
 									key={index}
@@ -104,8 +114,9 @@ export default class App extends Component {
 						value={this.state.currentItem.name}
 						onChange={(e) =>
 							this.setState((prevState) => {
+								//Sets the state of the current item every time the input changes
 								let currentItem = { ...prevState.currentItem };
-								currentItem.name = e.target.value;
+								currentItem.name = e.target.value; // Sets the name of the current item
 								return { currentItem };
 							})
 						}
@@ -116,8 +127,9 @@ export default class App extends Component {
 						value={this.state.currentItem.quantity}
 						onChange={(e) =>
 							this.setState((prevState) => {
+								// Sets the state of the current item every time the input changes
 								let currentItem = { ...prevState.currentItem };
-								currentItem.quantity = e.target.value;
+								currentItem.quantity = e.target.value; // Sets the quantity of the current item
 								return { currentItem };
 							})
 						}
@@ -130,13 +142,14 @@ export default class App extends Component {
 					<h1>
 						{
 							this.state.obiecte.filter((item) => item.completed === true)
-								.length
+								.length // Number of the items bought
 						}{' '}
-						Cumparate
+						cumparate
 					</h1>
 					{this.state.obiecte
 						.filter((item) => item.completed === true)
 						.map((item) => {
+							// Returns an Item for each completed item in the array
 							return (
 								<Item
 									key={item.key}
