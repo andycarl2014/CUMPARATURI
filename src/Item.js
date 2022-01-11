4143e743763c3fa49d7d74fb5025887d8b9b4cb1
andycarl2014 <innocentskz42@gmail.com>
2022-01-11 11:21:22 +0200

    dupa comentarii

--- src/Item.js
+++ src/Item.js
@@ -8,24 +8,38 @@ export default class Item extends Component {
 	};
 	render() {
 		return (
-			<div className={this.props.item.completed ? 'complet' : 'incomplet'}>
+			<div
+				className={this.props.item.completed ? 'complet' : 'incomplet'} // Sets the className based on the item.completed prop
+			>
 				<input
 					type='checkbox'
 					checked={this.props.item.completed}
-					onChange={() => this.props.handleChange(this.props.item.key)}
+					onChange={
+						() => this.props.handleChange(this.props.item.key)
+						//Changes the item.completed prop
+					}
 				/>
-				<p style={this.props.item.completed ? this.completedStyles : null}>
+				<p
+					style={this.props.item.completed ? this.completedStyles : null}
+					// If the item is completed it adds the style
+				>
 					{this.props.item.name} ------- {this.props.item.quantity} Bucati
 				</p>
 				<button
-					hidden={this.props.item.completed}
-					onClick={() => this.props.handleClickPlus(this.props.item.key)}
+					hidden={this.props.item.completed} // If the item is completed, hides the + button
+					onClick={
+						() => this.props.handleClickPlus(this.props.item.key)
+						// Adds the functionality for the + button
+					}
 				>
 					+
 				</button>
 				<button
-					hidden={this.props.item.completed}
-					onClick={() => this.props.handleClickMinus(this.props.item.key)}
+					hidden={this.props.item.completed} // If the item is completed, hides the - button
+					onClick={
+						() => this.props.handleClickMinus(this.props.item.key)
+						// Adds the functionality for the - button
+					}
 				>
 					-
 				</button>
